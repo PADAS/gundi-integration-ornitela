6bd75105d1c26c271d38d81f76697db668d98e48
Chris Doehring <chrisdoehring@gmail.com>
2025-10-30 12:44:57 -0700

    refactor: make env optional.

--- app/settings/integration.py
+++ app/settings/integration.py
@@ -1,4 +1,7 @@
 from environs import Env
+import logging
+
+logger = logging.getLogger(__name__)
 
 env = Env()
 env.read_env()
@@ -6,7 +9,6 @@ env.read_env()
 # Add your integration-specific settings here
 INFILE_STORAGE_BUCKET = env.str("INFILE_STORAGE_BUCKET", None)
 if INFILE_STORAGE_BUCKET is None:
-    raise RuntimeError(
-        "INFILE_STORAGE_BUCKET environment variable is not set. "
-        "Please set it to the name of your GCP bucket for file storage."
+    logger.warning(
+        "INFILE_STORAGE_BUCKET is not set; some features that require GCS may be disabled in this run."
     )
